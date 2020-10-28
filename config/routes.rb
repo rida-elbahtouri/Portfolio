54d8dd49984927c1b7d3a2a4b227bdb5ff4e1854
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-10-28 16:54:05 +0100

    use img url as header background

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :projects
   resources :emails
-  root to:"projects#new"
+  root to:"projects#index"
 end
