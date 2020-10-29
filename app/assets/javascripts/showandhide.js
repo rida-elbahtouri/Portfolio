049bf76caff18b2c07af83ddf9b5c71e89278282
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-10-29 14:03:28 +0100

    cghange bg img and fix navbar on scrolling

--- app/assets/javascripts/showandhide.js
+++ app/assets/javascripts/showandhide.js
@@ -1,13 +1,22 @@
 document.addEventListener("turbolinks:load", function() {
 
-let frontskill = document.getElementById('frontskills')
-let showfront = document.getElementById('showfrontskill')
-let backskill = document.getElementById('backendskills')
-let showbackend = document.getElementById('showbackendskill')
-let otherskills = document.getElementById('otherskills')
-let showotherskills = document.getElementById('showotherskill')
+const frontskill = document.getElementById('frontskills')
+const showfront = document.getElementById('showfrontskill')
+const backskill = document.getElementById('backendskills')
+const showbackend = document.getElementById('showbackendskill')
+const otherskills = document.getElementById('otherskills')
+const showotherskills = document.getElementById('showotherskill')
 
-
+window.onscroll = function() {scroll()};
+const nav= document.getElementById("mynav");
+var sticky = nav.offsetTop;
+const scroll=()=> {
+    if (window.pageYOffset > sticky) {
+      nav.classList.add("sticky");
+    } else {
+      nav.classList.remove("sticky");
+    }
+}
 
 function showHide(elm,id){
     let arrows = document.getElementById(id)
